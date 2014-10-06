86240b4e491355171205a12b15e08ddf2fcd585c
Alexey Kramarenko <alexeyk13@yandex.ru>
2014-10-06 11:27:20 +0400

    some STM32F1 examples: GPIO, UART, DAC, Temp, WDT, USB (CDC)

---
+++ example/stm32f1/kernel_config.h
@@ -0,0 +1,43 @@
+#ifndef KERNEL_CONFIG_H
+#define KERNEL_CONFIG_H
+
+//----------------------------------- kernel ------------------------------------------------------------------
+//enable kernel info. Disabling this you can save some flash size, but kernel will be much less verbose, especially on critical errors. Generally doesn't affect on perfomance
+#define KERNEL_INFO                                 1
+//marks objects with magic in headers. Decrease perfomance on few tacts, but very useful for debug if you don't have MPU enabled
+#define KERNEL_MARKS                                1
+//check range of dynamic objects in pools
+#define KERNEL_RANGE_CHECKING                       1
+//check kernel handles. Require few tacts, but making kernel calls much safer
+#define KERNEL_HANDLE_CHECKING                      1
+//check user adresses. Require few tacts, but making kernel calls much safer
+#define KERNEL_ADDRESS_CHECKING                     1
+//some kernel statistics (stack, mem, etc). Decrease perfomance in any object creation.
+#define KERNEL_PROFILING                            1
+//enable kernel assertions. If assertions happens, system will be halted with FILE and LINE, instead of returning error.
+//Doesn't affect on perfomance, but recommended only for debug
+#define KERNEL_ASSERTIONS                           1
+//halt system instead of reset. Only for development. Turn off in production.
+#define KERNEL_HALT_ON_FATAL_ERROR                  1
+//enable this only if you have problems with system timer. May decrease perfomance
+#define KERNEL_TIMER_DEBUG                          0
+//Enabling this you will get stats on each thread uptime, but decreasing context switching up to 2 times
+#define KERNEL_PROCESS_STAT                         0
+//Enable this only if you have problems with IPC oferflow.
+#define KERNEL_IPC_DEBUG                            0
+//maximum number of blocks, that can be opened at same time. Generally, it's MPU blocks count - 2
+#define KERNEL_BLOCKS_COUNT                         6
+//mutex, event, semaphore are now deprecated. Use stream, direct, block instead
+#define KERNEL_MES                                  0
+
+//size of GLOBAL consts. For MPU must be pow of 2, starting from 32
+//you must have good reason to changing this
+#define KERNEL_GLOBAL_SIZE                          32
+//total size of kernel RAM, including GLOBAL. For MPU Must be pow of 2, starting from 32
+#define KERNEL_SIZE                                 0x2000
+
+//save stdio, stdlib and time are required libs, all rest is optional
+#define LIB_ARRAY                                   1
+
+
+#endif // KERNEL_CONFIG_H
---
+++ example/stm32f1/main.c
@@ -0,0 +1,216 @@
+#include "../../rexos/userspace/process.h"
+#include "../../rexos/userspace/lib/time.h"
+#include "../../rexos/userspace/lib/stdlib.h"
+#include "../../rexos/userspace/lib/stdio.h"
+#include "../../rexos/userspace/timer.h"
+#include "../../rexos/userspace/ipc.h"
+#include "../../rexos/userspace/stream.h"
+#include "../../rexos/userspace/direct.h"
+#include "string.h"
+#include "../../rexos/sys/sys.h"
+#include "../../rexos/sys/drv/stm32_gpio.h"
+#include "../../rexos/sys/drv/stm32_uart.h"
+#include "../../rexos/sys/drv/stm32_rtc.h"
+#include "../../rexos/sys/drv/stm32_analog.h"
+#include "../../rexos/sys/drv/stm32_usb.h"
+#include "../../rexos/sys/midware/cdc.h"
+#include "../../rexos/sys/midware/usbd.h"
+#include "../../rexos/sys/usb.h"
+#include "../../rexos/sys/file.h"
+#include "usb_desc.h"
+
+#define TEST_ROUNDS                                 10000
+
+void app();
+void test_thread3();
+
+const REX __APP = {
+    //name
+    "App main",
+    //size
+    512,
+    //priority
+    120,
+    //flags
+    PROCESS_FLAGS_ACTIVE,
+    //ipc size
+    10,
+    //function
+    app
+};
+
+const REX test3 = {
+    //name
+    "test3",
+    //size
+    512,
+    //priority
+    119,
+    //flags
+    PROCESS_FLAGS_ACTIVE,
+    //ipc size
+    10,
+    //function
+    test_thread3
+};
+
+void test_thread3()
+{
+    TIME uptime;
+    open_stdout();
+    printf("test thread3 started\n\r");
+    printf("my name is: %s\n\r", __PROCESS_NAME);
+
+    HANDLE core = sys_get_object(SYS_OBJECT_CORE);
+    ack(core, STM32_GPIO_ENABLE_PIN, B9, PIN_MODE_OUT, 0);
+
+    ack(core, STM32_WDT_KICK, 0, 0, 0);
+    process_info();
+    bool set = true;
+    for (;;)
+    {
+        get_uptime(&uptime);
+        printf("uptime: %02d:%02d.%06d\n\r", uptime.sec / 60, uptime.sec % 60, uptime.usec);
+        printf("rtc: %d\n\r", get(core, STM32_RTC_GET, 0, 0, 0));
+        ack(core, STM32_GPIO_SET_PIN, B9, set, 0);
+        ack(core, STM32_WDT_KICK, 0, 0, 0);
+        set = !set;
+        sleep_ms(700);
+    }
+}
+
+void te_usb_disable_power()
+{
+    HANDLE core = sys_get_object(SYS_OBJECT_CORE);
+    ack(core, STM32_GPIO_ENABLE_PIN, C9, PIN_MODE_OUT, 0);
+    ack(core, STM32_GPIO_SET_PIN, C9, true, 0);
+}
+
+HANDLE usb_on()
+{
+    HANDLE usb, usbd, cdc;
+    CDC_OPEN_STRUCT cos;
+    te_usb_disable_power();
+    usb = process_create(&__STM32_USB);
+
+    //setup usbd
+    usbd = process_create(&__USBD);
+
+    fopen(usbd, usb);
+    direct_enable_read(usbd, (void*)&__DESCRIPTORS, __DESCRIPTORS.header.total_size);
+    ack(usbd, USBD_SETUP_DESCRIPTORS, USB_FULL_SPEED, __DESCRIPTORS.header.total_size, 0);
+    direct_enable_read(usbd, (void*)&__STRINGS, __STRINGS.header.header.total_size);
+    ack(usbd, USBD_SETUP_STRING_DESCRIPTORS, __STRINGS.header.header.total_size, 0, 0);
+
+    //setup cdc
+    cdc = process_create(&__CDC);
+    cos.data_ep = 1;
+    cos.control_ep = 2;
+    cos.data_ep_size = 64;
+    cos.control_ep_size = 16;
+    cos.rx_stream_size = cos.tx_stream_size = 32;
+    fopen_ex(cdc, usbd, (void*)&cos, sizeof(CDC_OPEN_STRUCT));
+
+    //turn USB on
+    fopen(usb, USB_HANDLE_DEVICE);
+
+    HANDLE rx_stream = get(cdc, IPC_GET_RX_STREAM, 0, 0, 0);
+    return stream_open(rx_stream);
+}
+
+void dac_on(HANDLE core, HANDLE analog)
+{
+    //TE pin enable power
+    ack(core, STM32_GPIO_ENABLE_PIN, C13, PIN_MODE_OUT, 0);
+    ack(core, STM32_GPIO_SET_PIN, C13, false, 0);
+
+    STM32_DAC_ENABLE de;
+    de.value = 1000;
+    de.flags = DAC_FLAGS_TIMER;
+    de.timer = TIM_6;
+    fopen_ex(analog, STM32_DAC1, &de, sizeof(STM32_DAC_ENABLE));
+}
+
+void dac_test(HANDLE analog)
+{
+    HANDLE block = block_create(1024);
+    int i;
+    uint32_t *ptr = block_open(block);
+    for (i = 0; i < 1024; ++i)
+        ptr[i] = 0xfff * (i & 1);
+    fwrite(analog, STM32_DAC1, block, 1024);
+    fwrite(analog, STM32_DAC1, block, 1024);
+}
+
+void app()
+{
+    HANDLE core, uart, analog, usb_rx;
+    core = sys_get_object(SYS_OBJECT_CORE);
+    TIME uptime;
+    int i;
+    unsigned int diff;
+    open_stdout();
+    open_stdin();
+
+    printf("App started\n\r");
+    ack(core, STM32_WDT_KICK, 0, 0, 0);
+
+    char c;
+
+    analog = process_create(&__STM32_ANALOG);
+    fopen(analog, STM32_ADC);
+
+    usb_rx = usb_on();
+
+    //first second signal may go faster
+    sleep_ms(1000);
+    ack(core, STM32_WDT_KICK, 0, 0, 0);
+    printf("Ping\n\r");
+    if (sys_ack(IPC_PING, 0, 0, 0))
+        printf("Pong!\n\r");
+    else
+        printf("err: %d\n\r", get_last_error());
+
+    get_uptime(&uptime);
+    for (i = 0; i < TEST_ROUNDS; ++i)
+        svc_test();
+    ack(core, STM32_WDT_KICK, 0, 0, 0);
+    diff = time_elapsed_us(&uptime);
+    printf("average kernel call time: %d.%dus\n\r", diff / TEST_ROUNDS, (diff / (TEST_ROUNDS / 10)) % 10);
+
+    get_uptime(&uptime);
+    for (i = 0; i < TEST_ROUNDS; ++i)
+        process_switch_test();
+    ack(core, STM32_WDT_KICK, 0, 0, 0);
+    diff = time_elapsed_us(&uptime);
+    printf("average switch time: %d.%dus\n\r", diff / TEST_ROUNDS, (diff / (TEST_ROUNDS / 10)) % 10);
+
+    get_uptime(&uptime);
+    for (i = 0; i < TEST_ROUNDS; ++i)
+        sys_ack(IPC_PING, 0, 0, 0);
+    ack(core, STM32_WDT_KICK, 0, 0, 0);
+    diff = time_elapsed_us(&uptime);
+    printf("average sys ping time: %d.%dus\n\r", diff / TEST_ROUNDS, (diff / (TEST_ROUNDS / 10)) % 10);
+
+    process_create(&test3);
+
+    uart = sys_get_object(SYS_OBJECT_UART);
+
+    ack(core, IPC_GET_INFO, 0, 0, 0);
+    //uart info in direct mode. Make sure all data is sent before
+    sleep_ms(50);
+    ack(uart, IPC_GET_INFO, 0, 0, 0);
+
+    dac_on(core, analog);
+    dac_test(analog);
+
+    int temp = get(analog, STM32_ADC_TEMP, 0, 0, 0);
+    printf("Temp (raw): %d.%d\n\r", temp / 10, temp % 10);
+
+
+    for (;;)
+    {
+        stream_read(usb_rx, &c, 1);
+        putc(c);
+    }
+}
