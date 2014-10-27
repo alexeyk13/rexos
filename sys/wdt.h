48a17f06dc03daf43ca44f6fcfbbcf5234adfa51
Alexey Kramarenko <alexeyk13@yandex.ru>
2014-10-27 19:11:58 +0400

    STM32 WDT HAL application driver

--- sys/wdt.h
+++ sys/wdt.h
@@ -1,4 +1,24 @@
+/*
+    RExOS - embedded RTOS
+    Copyright (c) 2011-2014, Alexey Kramarenko
+    All rights reserved.
+*/
+
 #ifndef WDT_H
 #define WDT_H
+
+#include "sys.h"
+
+typedef enum {
+    WDT_KICK = HAL_IPC(HAL_WDT),
+
+    WDT_HAL_MAX
+} WDT_IPCS;
+
+__STATIC_INLINE void wdt_kick()
+{
+    ack(object_get(SYS_OBJ_CORE), WDT_KICK, 0, 0, 0);
+}
+
 
 #endif // WDT_H
