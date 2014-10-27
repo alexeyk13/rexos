5d30b6696d6dd48c2ff01be4a20a0d80e71d5180
Alexey Kramarenko <alexeyk13@yandex.ru>
2014-10-27 16:23:37 +0400

    RTC HAL application abstraction

--- sys/rtc.h
+++ sys/rtc.h
@@ -11,6 +11,7 @@
 #include "sys_config.h"
 #include "../userspace/cc_macro.h"
 #include "../userspace/ipc.h"
+#include "../userspace/lib/time.h"
 
 typedef enum {
     RTC_GET = HAL_IPC(HAL_RTC),                             //!< Get RTC value
@@ -18,5 +19,15 @@ typedef enum {
 
     GPIO_RTC_MAX
 } RTC_IPCS;
+
+__STATIC_INLINE time_t rtc_get()
+{
+    return get(object_get(SYS_OBJ_CORE), RTC_GET, 0, 0, 0);
+}
+
+__STATIC_INLINE void rtc_set(time_t time)
+{
+    ack(object_get(SYS_OBJ_CORE), RTC_SET, (unsigned int)time, 0, 0);
+}
 
 #endif // RTC_H
