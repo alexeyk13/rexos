2e57c5d873ae9f6ccaa2e6d68fa2ef4636af4a00
Alexey Kramarenko <alexeyk13@yandex.ru>
2014-10-27 19:31:22 +0400

    minor fix

--- sys/drv/stm32_gpio.h
+++ sys/drv/stm32_gpio.h
@@ -129,7 +129,7 @@ typedef struct {
 void stm32_gpio_init(CORE* core);
 bool stm32_gpio_request(CORE* core, IPC* ipc);
 
-__STATIC_INLINE void stm32_gpio_request_inline(CORE* core, unsigned int cmd, unsigned int param1, unsigned int param2, unsigned int param3)
+__STATIC_INLINE unsigned int stm32_gpio_request_inline(CORE* core, unsigned int cmd, unsigned int param1, unsigned int param2, unsigned int param3)
 {
     IPC ipc;
     ipc.cmd = cmd;
@@ -137,6 +137,7 @@ __STATIC_INLINE void stm32_gpio_request_inline(CORE* core, unsigned int cmd, uns
     ipc.param2 = param2;
     ipc.param3 = param3;
     stm32_gpio_request(core, &ipc);
+    return ipc.param1;
 }
 
 
