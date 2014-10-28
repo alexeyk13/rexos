9a2cdd0b6cc1f0f530cc293e376e6bd98efd8aab
Alexey Kramarenko <alexeyk13@yandex.ru>
2014-10-28 19:49:50 +0300

    prepare core for monolith uart

--- sys/drv/stm32_core_private.h
+++ sys/drv/stm32_core_private.h
@@ -12,11 +12,18 @@
 #include "stm32_gpio.h"
 #include "stm32_timer.h"
 #include "stm32_power.h"
+#if (MONOLITH_UART)
+#include "stm32_uart.h"
+#endif
+
 
 typedef struct _CORE {
     GPIO_DRV gpio;
     TIMER_DRV timer;
     POWER_DRV power;
+#if (MONOLITH_UART)
+    UART_DRV uart;
+#endif
 }CORE;
 
 #endif // STM32_CORE_PRIVATE_H
