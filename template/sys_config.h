62e92f64225b1cf5be8e3dbc9e761c347e8703d5
Alexey Kramarenko <alexeyk13@yandex.ru>
2014-12-12 19:57:37 +0300

    template update for new sys objects

--- template/sys_config.h
+++ template/sys_config.h
@@ -12,24 +12,30 @@
  */
 
 //add some debug info in SYS
-#define SYS_INFO                                            1
+#define SYS_INFO                                            0
 //----------------------------- objects ----------------------------------------------
 //make sure, you know what are you doing, before change
 #define SYS_OBJ_STDOUT                                      0
-#define SYS_OBJ_STDIN                                       1
-#define SYS_OBJ_CORE                                        2
-#define SYS_OBJ_UART                                        3
+#define SYS_OBJ_CORE                                        1
+#define SYS_OBJ_USBD                                        2
 
-#define STDIO_STREAM_SIZE                                   32
+#define SYS_OBJ_I2C                                         SYS_OBJ_CORE
+#define SYS_OBJ_UART                                        SYS_OBJ_CORE
+#define SYS_OBJ_USB                                         SYS_OBJ_CORE
+#define SYS_OBJ_STDIN                                       INVALID_HANDLE
+//------------------------------ stdio -----------------------------------------------
+#define STDIO_STREAM_SIZE                                   16
 //-------------------------------- USB -----------------------------------------------
 #define USB_EP_COUNT_MAX                                    3
 //low-level USB debug. Turn on only in case of IO problems
 #define USB_DEBUG_REQUESTS                                  0
 #define USB_DEBUG_CLASS_REQUESTS                            0
 #define USB_DEBUG_ERRORS                                    0
+//USB test mode support. Not all hardware supported.
+#define USB_TEST_MODE                                       0
 
 //Sizeof USB device process. Remember, that process itself requires around 512 bytes
-#define USB_DEVICE_PROCESS_SIZE                             620
+#define USB_DEVICE_PROCESS_SIZE                             600
 #define USB_CDC_PROCESS_SIZE                                450
 
 #endif // SYS_CONFIG_H
