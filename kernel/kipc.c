92bdae7758836282c8f2b89c2563ca30c3b935b5
Alexey Kramarenko <alexeyk13@yandex.ru>
2015-03-04 11:54:07 +0300

    IPC wait for exact command in call

--- kernel/kipc.c
+++ kernel/kipc.c
@@ -13,22 +13,24 @@
 
 #define IPC_ITEM(process, num)              ((IPC*)((unsigned int)(process) + sizeof(PROCESS) + (num) * sizeof(IPC)))
 
+#define ANY_CMD                             0xffffffff
+
 void kipc_lock_release(HANDLE process)
 {
     ((PROCESS*)process)->kipc.wait_process = INVALID_HANDLE;
 }
 
-static inline int kipc_index(PROCESS* process, HANDLE wait_process)
+static inline int kipc_index(PROCESS* process, HANDLE wait_process, unsigned int cmd)
 {
     int i;
     unsigned int head = process->kipc.rb.head;
     for (i = process->kipc.rb.tail; i != head; i = RB_ROUND(&process->kipc.rb, i + 1))
-        if ((IPC_ITEM(process, i)->process == wait_process) || (wait_process == ANY_HANDLE))
+        if (((IPC_ITEM(process, i)->process == wait_process) || (wait_process == ANY_HANDLE)) && ((IPC_ITEM(process, i)->cmd == cmd) || (cmd == ANY_CMD)))
             return i;
     return -1;
 }
 
-void kipc_read_process(PROCESS* process, IPC* ipc, TIME* time, HANDLE wait_process)
+void kipc_read_process(PROCESS* process, IPC* ipc, TIME* time, HANDLE wait_process, unsigned int cmd)
 {
     int i;
     IPC tmp;
@@ -42,8 +44,11 @@ void kipc_read_process(PROCESS* process, IPC* ipc, TIME* time, HANDLE wait_proce
     kprocess_sleep(process, time, PROCESS_SYNC_IPC, process);
 
     disable_interrupts();
-    if ((i = kipc_index(process, wait_process)) < 0)
+    if ((i = kipc_index(process, wait_process, cmd)) < 0)
+    {
         process->kipc.wait_process = wait_process;
+        process->kipc.cmd = cmd;
+    }
     enable_interrupts();
 
     //maybe already on queue? Peek.
@@ -73,8 +78,11 @@ void kipc_post_process(IPC* ipc, unsigned int sender)
     CHECK_HANDLE(receiver, sizeof(PROCESS));
     CHECK_MAGIC(receiver, MAGIC_PROCESS);
     disable_interrupts();
-    if ((wake = (receiver->kipc.wait_process == sender || receiver->kipc.wait_process == ANY_HANDLE)))
+    if ((wake = ((receiver->kipc.wait_process == sender || receiver->kipc.wait_process == ANY_HANDLE) && (receiver->kipc.cmd == ipc->cmd || receiver->kipc.cmd == ANY_CMD))))
+    {
         receiver->kipc.wait_process = INVALID_HANDLE;
+        receiver->kipc.cmd = ANY_CMD;
+    }
     else if (!rb_is_full(&receiver->kipc.rb))
         cur = IPC_ITEM(receiver, rb_put(&receiver->kipc.rb));
     enable_interrupts();
@@ -121,6 +129,7 @@ void kipc_init(HANDLE handle, int size)
         size = 1;
     rb_init(&(process->kipc.rb), size);
     process->kipc.wait_process = INVALID_HANDLE;
+    process->kipc.cmd = ANY_CMD;
 }
 
 void kipc_post(IPC* ipc)
@@ -132,7 +141,7 @@ void kipc_post(IPC* ipc)
 void kipc_read(IPC* ipc, TIME* time, HANDLE wait_process)
 {
     PROCESS* process = kprocess_get_current();
-    kipc_read_process(process, ipc, time, wait_process);
+    kipc_read_process(process, ipc, time, wait_process, ANY_CMD);
 }
 
 void kipc_call(IPC* ipc, TIME* time)
@@ -141,5 +150,5 @@ void kipc_call(IPC* ipc, TIME* time)
     CHECK_ADDRESS(process, ipc, sizeof(IPC));
     HANDLE wait_process = (HANDLE)(ipc->process);
     kipc_post_process(ipc, (HANDLE)process);
-    kipc_read_process(process, ipc, time, wait_process);
+    kipc_read_process(process, ipc, time, wait_process, ipc->cmd);
 }
