cc7f17e45b64ba5d9e77a049a199aabeb325528f
Alexey Kramarenko <alexeyk13@yandex.ru>
2015-04-28 17:14:58 +0300

    HAL power interface

---
+++ userspace/power.h
@@ -0,0 +1,33 @@
+/*
+    RExOS - embedded RTOS
+    Copyright (c) 2011-2015, Alexey Kramarenko
+    All rights reserved.
+*/
+
+#ifndef POWER_H
+#define POWER_H
+
+#include "sys.h"
+#include "file.h"
+#include "cc_macro.h"
+#include "sys_config.h"
+
+typedef enum {
+    POWER_MODE_HIGH = 0,
+    POWER_MODE_LOW,
+    POWER_MODE_ULTRA_LOW,
+    POWER_MODE_STOP,
+    POWER_MODE_STANDY
+} POWER_MODE;
+
+typedef enum {
+    POWER_SET_MODE = HAL_IPC(HAL_POWER),
+    POWER_MAX
+} POWER_IPCS;
+
+__STATIC_INLINE void power_set_mode(POWER_MODE mode)
+{
+    ack(object_get(SYS_OBJ_CORE), POWER_SET_MODE, mode, 0, 0);
+}
+
+#endif // POWER_H
