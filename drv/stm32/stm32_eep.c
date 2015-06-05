41cec8dee7e21143d00691b180b377d72bca6ab1
Alexey Kramarenko <alexeyk13@yandex.ru>
2015-06-05 15:02:41 +0300

    stm32 eep io interface

--- drv/stm32/stm32_eep.c
+++ drv/stm32/stm32_eep.c
@@ -7,8 +7,7 @@
 #include "stm32_eep.h"
 #include "stm32_core_private.h"
 #include "stm32_config.h"
-#include "../../userspace/direct.h"
-#include "../../userspace/file.h"
+#include "../../userspace/io.h"
 #include "string.h"
 
 #define PEKEY1                          0x89ABCDEF
@@ -27,65 +26,51 @@ static inline void stm32_eep_seek(CORE* core, unsigned int addr)
     core->eep.addr = addr;
 }
 
-static inline void stm32_eep_read(CORE* core, unsigned int size, HANDLE process)
+static inline void stm32_eep_read(CORE* core, IPC* ipc)
 {
-    unsigned int chunk_size, processed;
-    uint32_t buf[STM32_EEPROM_BUF_SIZE / 4];
-    if (core->eep.addr + size > EEP_BASE + EEP_SIZE)
+    IO* io = (IO*)ipc->param2;
+    io->data_size = 0;
+    if (core->eep.addr + ipc->param3 > EEP_BASE + EEP_SIZE)
     {
-        error(ERROR_OUT_OF_RANGE);
+        io_send_error(ipc, ERROR_OUT_OF_RANGE);
         return;
     }
-    for (processed = 0; processed < size; processed += chunk_size)
+    if (IO_FREE(io) < ipc->param3)
     {
-        chunk_size = size - processed;
-        if (chunk_size > STM32_EEPROM_BUF_SIZE)
-            chunk_size = STM32_EEPROM_BUF_SIZE;
-        memcpy(buf, (void*)(core->eep.addr + processed), chunk_size);
-        if (!direct_write(process, buf, chunk_size))
-        {
-            fread_complete(process, HAL_EEPROM, 0, INVALID_HANDLE, get_last_error());
-            return;
-        }
+        io_send_error(ipc, ERROR_IO_BUFFER_TOO_SMALL);
+        return;
     }
-    fread_complete(process, HAL_EEPROM, 0, INVALID_HANDLE, processed);
+    memcpy(IO_DATA(io), (void*)core->eep.addr, ipc->param3);
+    io->data_size = ipc->param3;
+    io_send(ipc);
 }
 
-static inline void stm32_eep_write(CORE* core, unsigned int size, HANDLE process)
+static inline void stm32_eep_write(CORE* core, IPC* ipc)
 {
-    unsigned int chunk_size, processed, i;
-    uint8_t buf[STM32_EEPROM_BUF_SIZE];
-    if (core->eep.addr + size > EEP_BASE + EEP_SIZE)
+    unsigned int i;
+    IO* io = (IO*)ipc->param2;
+    if (core->eep.addr + io->data_size > EEP_BASE + EEP_SIZE)
     {
-        error(ERROR_OUT_OF_RANGE);
+        io_send_error(ipc, ERROR_OUT_OF_RANGE);
         return;
     }
     FLASH->PECR &= ~(FLASH_PECR_FPRG | FLASH_PECR_ERASE | FLASH_PECR_FTDW | FLASH_PECR_PROG);
     FLASH->PECR |= FLASH_PECR_DATA;
-    for (processed = 0; processed < size; processed += chunk_size)
+    for (i = 0; i < io->data_size; i += sizeof(uint32_t))
     {
-        chunk_size = size - processed;
-        if (chunk_size > STM32_EEPROM_BUF_SIZE)
-            chunk_size = STM32_EEPROM_BUF_SIZE;
-        if (!direct_read(process, buf, chunk_size))
-        {
-            fwrite_complete(process, HAL_EEPROM, 0, INVALID_HANDLE, get_last_error());
-            return;
-        }
+        while (FLASH->SR & FLASH_SR_BSY) {}
 
-        for (i = 0; i < chunk_size; i += sizeof(uint32_t))
-        {
-            while (FLASH->SR & FLASH_SR_BSY) {}
-            *(uint32_t*)(core->eep.addr + processed + i) = *((uint32_t*)(buf + i));
-        }
+        *(uint32_t*)(core->eep.addr + i) = *((uint32_t*)(IO_DATA(io) + i));
+
         if (FLASH->SR & (FLASH_SR_FWWERR | FLASH_SR_NOTZEROERR | FLASH_SR_SIZERR | FLASH_SR_PGAERR))
         {
             FLASH->SR |= FLASH_SR_FWWERR | FLASH_SR_NOTZEROERR | FLASH_SR_SIZERR | FLASH_SR_PGAERR;
-            fwrite_complete(process, HAL_EEPROM, 0, INVALID_HANDLE, ERROR_HARDWARE);
+            io_send_error(ipc, ERROR_HARDWARE);
             return;
         }
     }
-    fwrite_complete(process, HAL_EEPROM, 0, INVALID_HANDLE, processed);
+    ipc->param3 = io->data_size;
+    io_send(ipc);
 }
 
 bool stm32_eep_request(CORE* core, IPC* ipc)
@@ -98,10 +83,10 @@ bool stm32_eep_request(CORE* core, IPC* ipc)
         need_post = true;
         break;
     case IPC_READ:
-        stm32_eep_read(core, ipc->param3, ipc->process);
+        stm32_eep_read(core, ipc);
         break;
     case IPC_WRITE:
-        stm32_eep_write(core, ipc->param3, ipc->process);
+        stm32_eep_write(core, ipc);
         break;
     }
     return need_post;
