2f4e65a8fa3ed6531ab73ec5c6d5020758cd1f80
Alexey Kramarenko <alexeyk13@yandex.ru>
2015-06-10 11:27:31 +0300

    lpc eeprom io interface

--- drv/lpc/lpc_eep.c
+++ drv/lpc/lpc_eep.c
@@ -7,8 +7,7 @@
 #include "lpc_eep.h"
 #include "lpc_core_private.h"
 #include "lpc_config.h"
-#include "../../userspace/direct.h"
-#include "../../userspace/file.h"
+#include "../../userspace/io.h"
 #include "lpc_power.h"
 
 #define IAP_LOCATION 0x1FFF1FF1
@@ -20,66 +19,43 @@ static const IAP iap = (IAP)IAP_LOCATION;
 #define IAP_CMD_WRITE_EEPROM                        61
 #define IAP_CMD_READ_EEPROM                         62
 
-static inline void lpc_eep_read(CORE* core, unsigned int size, HANDLE process)
+static inline void lpc_eep_read(CORE* core, IPC* ipc)
 {
-    unsigned int chunk_size, processed;
     unsigned int req[5];
     unsigned int resp[1];
-    uint8_t buf[LPC_EEPROM_BUF_SIZE];
-    for (processed = 0; processed < size; processed += chunk_size)
+    IO* io = (IO*)ipc->param2;
+    req[0] = IAP_CMD_READ_EEPROM;
+    req[1] = core->eep.addr;
+    req[2] = (unsigned int)io_data(io);
+    req[3] = ipc->param3;
+    req[4] = lpc_power_get_system_clock_inside(core) / 1000;
+    iap(req, resp);
+    if (resp[0] != 0)
     {
-        chunk_size = size - processed;
-        if (chunk_size > LPC_EEPROM_BUF_SIZE)
-            chunk_size = LPC_EEPROM_BUF_SIZE;
-        req[0] = IAP_CMD_READ_EEPROM;
-        req[1] = core->eep.addr + processed;
-        req[2] = (unsigned int)(&buf);
-        req[3] = chunk_size;
-        req[4] = lpc_power_get_system_clock_inside(core) / 1000;
-        iap(req, resp);
-        if (resp[0] != 0)
-        {
-            fread_complete(process, HAL_EEPROM, 0, INVALID_HANDLE, ERROR_INVALID_PARAMS);
-            return;
-        }
-        if (!direct_write(process, buf, chunk_size))
-        {
-            fread_complete(process, HAL_EEPROM, 0, INVALID_HANDLE, get_last_error());
-            return;
-        }
+        io_send_error(ipc, ERROR_INVALID_PARAMS);
+        return;
     }
-    fread_complete(process, HAL_EEPROM, 0, INVALID_HANDLE, processed);
+    io->data_size = ipc->param1;
+    io_send(ipc);
 }
 
-static inline void lpc_eep_write(CORE* core, unsigned int size, HANDLE process)
+static inline void lpc_eep_write(CORE* core, IPC* ipc)
 {
-    unsigned int chunk_size, processed;
     unsigned int req[5];
     unsigned int resp[1];
-    uint8_t buf[LPC_EEPROM_BUF_SIZE];
-    for (processed = 0; processed < size; processed += chunk_size)
+    IO* io = (IO*)ipc->param2;
+    req[0] = IAP_CMD_WRITE_EEPROM;
+    req[1] = core->eep.addr;
+    req[2] = (unsigned int)io_data(io);
+    req[3] = io->data_size;
+    req[4] = lpc_power_get_system_clock_inside(core) / 1000;
+    iap(req, resp);
+    if (resp[0] != 0)
     {
-        chunk_size = size - processed;
-        if (chunk_size > LPC_EEPROM_BUF_SIZE)
-            chunk_size = LPC_EEPROM_BUF_SIZE;
-        if (!direct_read(process, buf, chunk_size))
-        {
-            fwrite_complete(process, HAL_EEPROM, 0, INVALID_HANDLE, get_last_error());
-            return;
-        }
-        req[0] = IAP_CMD_WRITE_EEPROM;
-        req[1] = core->eep.addr + processed;
-        req[2] = (unsigned int)(&buf);
-        req[3] = chunk_size;
-        req[4] = lpc_power_get_system_clock_inside(core) / 1000;
-        iap(req, resp);
-        if (resp[0] != 0)
-        {
-            fwrite_complete(process, HAL_EEPROM, 0, INVALID_HANDLE, ERROR_INVALID_PARAMS);
-            return;
-        }
+        io_send_error(ipc, ERROR_INVALID_PARAMS);
+        return;
     }
-    fwrite_complete(process, HAL_EEPROM, 0, INVALID_HANDLE, processed);
+    io_send(ipc);
 }
 
 bool lpc_eep_request(CORE* core, IPC* ipc)
@@ -92,10 +68,10 @@ bool lpc_eep_request(CORE* core, IPC* ipc)
         need_post = true;
         break;
     case IPC_READ:
-        lpc_eep_read(core, ipc->param3, ipc->process);
+        lpc_eep_read(core, ipc);
         break;
     case IPC_WRITE:
-        lpc_eep_write(core, ipc->param3, ipc->process);
+        lpc_eep_write(core, ipc);
         break;
     }
     return need_post;
