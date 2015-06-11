10dd0480f526c5a0607dcbe3d45c53e4950f4efd
Alexey Kramarenko <alexeyk13@yandex.ru>
2015-06-11 14:19:31 +0300

    mass storage class

--- midware/usbd/mscd.c
+++ midware/usbd/mscd.c
@@ -11,6 +11,8 @@
 #include "../../userspace/uart.h"
 #include "../../userspace/stdlib.h"
 #include "../../userspace/msc.h"
+#include "../../userspace/scsi.h"
+#include "../scsis.h"
 #include <stdint.h>
 #include "usb.h"
 #include "sys_config.h"
@@ -18,6 +20,7 @@
 typedef enum  {
     MSCD_STATE_CBW = 0,
     MSCD_STATE_DATA_IN,
+    MSCD_STATE_DATA_IN_ZLP,
     MSCD_STATE_STORAGE,
     MSCD_STATE_DATA_OUT,
     MSCD_STATE_CSW
@@ -28,16 +31,17 @@ typedef struct {
     uint16_t ep_size;
     IO* control;
     IO* data;
-
-    //in feature this will make LUN
     MSCD_STATE state;
-    IO* in;
-    IO* out;
-    uint32_t tag;
+    unsigned int residue;
+    SCSIS_RESPONSE resp;
+
+    //for multiple LUN each scsi for each LUN
+    SCSIS* scsis;
 } MSCD;
 
 static void mscd_destroy(MSCD* mscd)
 {
+    scsis_destroy(mscd->scsis);
     io_destroy(mscd->control);
     io_destroy(mscd->data);
     free(mscd);
@@ -47,6 +51,15 @@ static void mscd_fatal(USBD* usbd, MSCD* mscd)
 {
     usbd_usb_ep_set_stall(usbd, mscd->ep_num);
     usbd_usb_ep_set_stall(usbd, USB_EP_IN | mscd->ep_num);
+    mscd->state = MSCD_STATE_CBW;
+}
+
+static void mscd_flush(USBD* usbd, MSCD* mscd)
+{
+    usbd_usb_ep_flush(usbd, mscd->ep_num);
+    usbd_usb_ep_flush(usbd, USB_EP_IN | mscd->ep_num);
+    scsis_reset(mscd->scsis);
+    mscd->state = MSCD_STATE_CBW;
 }
 
 void mscd_class_configured(USBD* usbd, USB_CONFIGURATION_DESCRIPTOR_TYPE* cfg)
@@ -80,12 +93,11 @@ void mscd_class_configured(USBD* usbd, USB_CONFIGURATION_DESCRIPTOR_TYPE* cfg)
     mscd->ep_num = ep_num;
     mscd->ep_size = ep_size;
 
-    mscd->in = mscd->out = NULL;
     mscd->state = MSCD_STATE_CBW;
     mscd->control = io_create(ep_size);
-    //TODO: scsi stack size
-    mscd->data = io_create(USBD_MSC_MAX_IO_SIZE);
-    if (mscd->control == NULL || mscd->data == NULL)
+    mscd->data = io_create(USBD_MSC_MAX_IO_SIZE + sizeof(SCSI_STACK));
+    mscd->scsis = scsis_create();
+    if (mscd->control == NULL || mscd->data == NULL || mscd->scsis == NULL)
     {
         mscd_destroy(mscd);
         return;
@@ -108,21 +120,23 @@ void mscd_class_reset(USBD* usbd, void* param)
 {
     MSCD* mscd = (MSCD*)param;
 
-    //TODO:
-
+    usbd_unregister_endpoint(usbd, mscd->iface_num, mscd->ep_num);
+    usbd_unregister_interface(usbd, mscd->iface_num, &__MSCD_CLASS);
+    usbd_usb_ep_close(usbd, mscd->ep_num);
+    usbd_usb_ep_close(usbd, USB_EP_IN | mscd->ep_num);
     mscd_destroy(mscd);
 }
 
 void mscd_class_suspend(USBD* usbd, void* param)
 {
     MSCD* mscd = (MSCD*)param;
-    //TODO:
+    mscd_flush(usbd, mscd);
 }
 
 void mscd_class_resume(USBD* usbd, void* param)
 {
     MSCD* mscd = (MSCD*)param;
-    //TODO:
+    usbd_usb_ep_read(usbd, mscd->ep_num, mscd->control, mscd->ep_size);
 }
 
 static inline int mscd_get_max_lun(USBD* usbd, MSCD* mscd, IO* io)
@@ -135,9 +149,10 @@ static inline int mscd_get_max_lun(USBD* usbd, MSCD* mscd, IO* io)
     return sizeof(uint8_t);
 }
 
-static inline int mscd_mass_storage_reset(USBD* usbd, MSCD* mscd)
+static int mscd_mass_storage_reset(USBD* usbd, MSCD* mscd)
 {
-    //TODO:
+    mscd_flush(usbd, mscd);
+    usbd_usb_ep_read(usbd, mscd->ep_num, mscd->control, mscd->ep_size);
 #if (USBD_MSC_DEBUG_REQUESTS)
     printf("MSCD: Mass Storage Reset\n\r");
 #endif //USBD_CDC_DEBUG_REQUESTS
@@ -160,10 +175,62 @@ int mscd_class_setup(USBD* usbd, void* param, SETUP* setup, IO* io)
     return res;
 }
 
+static void mscd_csw(USBD* usbd, MSCD* mscd)
+{
+    uint32_t tag;
+    CBW* cbw = io_data(mscd->control);
+    CSW* csw = io_data(mscd->control);
+    tag = cbw->dCBWTag;
+
+    csw->dCSWSignature = MSC_CSW_SIGNATURE;
+    csw->dCSWTag = tag;
+    csw->dCSWDataResidue = mscd->residue;
+    switch (mscd->resp)
+    {
+    case SCSIS_RESPONSE_PASS:
+        csw->bCSWStatus = MSC_CSW_COMMAND_PASSED;
+        break;
+    case SCSIS_RESPONSE_FAIL:
+        csw->bCSWStatus = MSC_CSW_COMMAND_FAILED;
+        break;
+    default:
+        csw->bCSWStatus = MSC_CSW_PHASE_ERROR;
+    }
+    mscd->control->data_size = sizeof(CSW);
+    mscd->state = MSCD_STATE_CSW;
+    usbd_usb_ep_write(usbd, mscd->ep_num, mscd->control);
+}
+
+static void mscd_request_processed(USBD* usbd, MSCD* mscd)
+{
+    CBW* cbw = io_data(mscd->control);
+    if (MSC_CBW_FLAG_DATA_IN(cbw->bmCBWFlags) && cbw->dCBWDataTransferLength)
+    {
+        mscd->state = MSCD_STATE_DATA_IN;
+        if (mscd->data->data_size < cbw->dCBWDataTransferLength)
+        {
+            mscd->residue = cbw->dCBWDataTransferLength - mscd->data->data_size;
+            if (mscd->data->data_size == 0)
+                mscd->state = MSCD_STATE_DATA_IN_ZLP;
+        }
+        mscd->data->data_size = cbw->dCBWDataTransferLength - mscd->residue;
+        usbd_usb_ep_write(usbd, mscd->ep_num, mscd->data);
+    }
+    else
+        mscd_csw(usbd, mscd);
+}
+
 static void mscd_scsi_request(USBD* usbd, MSCD* mscd)
 {
-    printd("time for scsi");
-
+    CBW* cbw = io_data(mscd->control);
+    mscd->resp = scsis_request(mscd->scsis, cbw->CBWCB, cbw->bCBWCBLength, mscd->data);
+    if (mscd->resp == SCSIS_RESPONSE_STORAGE_REQUEST)
+    {
+        mscd->state = MSCD_STATE_STORAGE;
+        usbd_io_user(usbd, mscd->iface_num, cbw->bCBWLUN, HAL_CMD(HAL_USBD_IFACE, USB_MSC_STORAGE_REQUEST), mscd->control, 0);
+    }
+    else
+        mscd_request_processed(usbd, mscd);
 }
 
 static inline void mscd_cbw(USBD* usbd, MSCD* mscd)
@@ -176,25 +243,24 @@ static inline void mscd_cbw(USBD* usbd, MSCD* mscd)
 #endif //USBD_CDC_DEBUG_REQUESTS
         mscd_fatal(usbd, mscd);
     }
-    mscd->tag = cbw->dCBWTag;
 #if (USBD_MSC_DEBUG_IO)
     printf("USB_MSC: dCBWTag: %d\n\r", cbw->dCBWTag);
     printf("USB_MSC: dCBWDataTransferLength: %d\n\r", cbw->dCBWDataTransferLength);
     printf("USB_MSC: dCBWDataFlags: %02x\n\r", cbw->bmCBWFlags);
     printf("USB_MSC: dCBWLUN: %d\n\r", cbw->bCBWLUN);
-    //remove me
-    printf("USB_MSC: dCBWCB:");
-    int i;
-    for (i = 0; i < cbw->bCBWCBLength; ++i)
-        printf(" %02x", cbw->CBWCB[i]);
-    printf(" (%d)\n\r", cbw->bCBWCBLength);
 #endif //USBD_MSC_DEBUG_IO
+    mscd->residue = 0;
     if (MSC_CBW_FLAG_DATA_IN(cbw->bmCBWFlags) || cbw->dCBWDataTransferLength == 0)
+    {
+        mscd->data->data_size = 0;
         mscd_scsi_request(usbd, mscd);
+    }
     else
     {
         mscd->state = MSCD_STATE_DATA_OUT;
-        usbd_usb_ep_read(usbd, mscd->ep_num, mscd->data, cbw->dCBWDataTransferLength);
+        if (cbw->dCBWDataTransferLength > USBD_MSC_MAX_IO_SIZE)
+            mscd->residue = cbw->dCBWDataTransferLength - USBD_MSC_MAX_IO_SIZE;
+        usbd_usb_ep_read(usbd, mscd->ep_num, mscd->data, cbw->dCBWDataTransferLength - mscd->residue);
     }
 }
 
@@ -210,7 +276,35 @@ static inline void mscd_usb_rx(USBD* usbd, MSCD* mscd)
         break;
     default:
 #if (USBD_MSC_DEBUG_REQUESTS)
-        printf("MSCD: Fatal - invalid stage - %d\n\r", mscd->state);
+        printf("MSCD: invalid stage on read - %d\n\r", mscd->state);
+#endif //USBD_CDC_DEBUG_REQUESTS
+        mscd_fatal(usbd, mscd);
+    }
+}
+
+static inline void mscd_usb_tx(USBD* usbd, MSCD* mscd)
+{
+    switch (mscd->state)
+    {
+    case MSCD_STATE_CSW:
+        mscd->state = MSCD_STATE_CBW;
+        usbd_usb_ep_read(usbd, mscd->ep_num, mscd->control, mscd->ep_size);
+        break;
+    case MSCD_STATE_DATA_IN:
+        if (mscd->residue && ((mscd->data->data_size % mscd->ep_size) == 0))
+        {
+            mscd->state = MSCD_STATE_DATA_IN_ZLP;
+            mscd->data->data_size = 0;
+            usbd_usb_ep_write(usbd, mscd->ep_num, mscd->data);
+            break;
+        }
+        //follow down
+    case MSCD_STATE_DATA_IN_ZLP:
+        mscd_csw(usbd, mscd);
+        break;
+    default:
+#if (USBD_MSC_DEBUG_REQUESTS)
+        printf("MSCD: invalid stage on write - %d\n\r", mscd->state);
 #endif //USBD_CDC_DEBUG_REQUESTS
         mscd_fatal(usbd, mscd);
     }
@@ -225,13 +319,34 @@ static inline bool mscd_driver_event(USBD* usbd, MSCD* mscd, IPC* ipc)
         mscd_usb_rx(usbd, mscd);
         break;
     case IPC_WRITE:
-        printd("todo\n\r");
+        mscd_usb_tx(usbd, mscd);
         break;
     default:
         error(ERROR_NOT_SUPPORTED);
         need_post = true;
     }
     return need_post;
+}
+
+static inline void mscd_storage_response(USBD* usbd, MSCD* mscd, int param3)
+{
+    if (mscd->state != MSCD_STATE_STORAGE)
+    {
+        //generally not error - response can arrive after mass storage reset
+        mscd_flush(usbd, mscd);
+        usbd_usb_ep_read(usbd, mscd->ep_num, mscd->control, mscd->ep_size);
+        return;
+    }
+    if (param3 < 0)
+    {
+        mscd->resp = SCSIS_RESPONSE_PHASE_ERROR;
+        mscd_csw(usbd, mscd);
+    }
+    else
+    {
+        mscd->resp = scsis_storage_response(mscd->scsis, mscd->data);
+        mscd_csw(usbd, mscd);
+    }
 }
 
 bool mscd_class_request(USBD* usbd, void* param, IPC* ipc)
@@ -243,7 +358,9 @@ bool mscd_class_request(USBD* usbd, void* param, IPC* ipc)
     else
         switch (HAL_ITEM(ipc->cmd))
         {
-        //TODO: interface request
+        case USB_MSC_STORAGE_REQUEST:
+            mscd_storage_response(usbd, mscd, (int)ipc->param3);
+            break;
         default:
             error(ERROR_NOT_SUPPORTED);
             need_post = true;
--- userspace/msc.h
+++ userspace/msc.h
@@ -1,5 +1,13 @@
+/*
+    RExOS - embedded RTOS
+    Copyright (c) 2011-2015, Alexey Kramarenko
+    All rights reserved.
+*/
+
 #ifndef MSC_H
 #define MSC_H
+
+#include "ipc.h"
 
 #define MSC_INTERFACE_CLASS                                                             0x08
 
@@ -23,23 +31,33 @@
 #define MSC_CBW_FLAG_DATA_IN(flags)                                                     ((flags) & (1 << 7))
 
 typedef struct {
-    uint32_t	dCBWSignature;
-    uint32_t dCBWTag;
-    uint32_t	dCBWDataTransferLength;
-    uint8_t	bmCBWFlags;
-    uint8_t	bCBWLUN;
-    uint8_t	bCBWCBLength;
-    uint8_t	CBWCB[16];
+    uint32_t    dCBWSignature;
+    uint32_t    dCBWTag;
+    uint32_t    dCBWDataTransferLength;
+    uint8_t     bmCBWFlags;
+    uint8_t     bCBWLUN;
+    uint8_t     bCBWCBLength;
+    uint8_t     CBWCB[16];
 }CBW;
 
+#define MSC_CSW_SIGNATURE                                                               0x53425355
+
+#define MSC_CSW_COMMAND_PASSED                                                          0x0
+#define MSC_CSW_COMMAND_FAILED                                                          0x1
+#define MSC_CSW_PHASE_ERROR                                                             0x2
+
 typedef struct {
-    uint32_t	dCSWSignature;
-    uint32_t dCSWTag;
-    uint32_t	dCSWDataResidue;
-    uint8_t	bCSWStatus;
+    uint32_t    dCSWSignature;
+    uint32_t    dCSWTag;
+    uint32_t    dCSWDataResidue;
+    uint8_t     bCSWStatus;
 }CSW;
 
 #pragma pack(pop)
 
+typedef enum {
+    //requests from host
+    USB_MSC_STORAGE_REQUEST = IPC_USER
+} USB_MSC_IPC;
 
 #endif // MSC_H
