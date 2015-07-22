65d30bbcd2c4aa34c0a6153005e9f0b81d4371a2
Alexey Kramarenko <alexeyk13@yandex.ru>
2015-07-22 12:27:07 +0300

    LPC 18xx architecture decode

--- userspace/core/lpc.h
+++ userspace/core/lpc.h
@@ -7,7 +7,7 @@
 #ifndef LPC_H
 #define LPC_H
 
-//---------------------------------------------------------------------------- LPC11Uxx ----------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------ LPC11Uxx -------------------------------------------------------------------------------------
 #if defined(LPC11U12FBD48_201) || defined(LPC11U12FHN33_201) || defined(LPC11U22FBD48_201)
 #define LPC11U12
 #endif
@@ -28,7 +28,8 @@
 #define LPC11U23
 #endif
 
-#if defined(LPC11U24FET48_301) || defined(LPC11U24FHI33_301) || defined(LPC11U24FBD48_301) || defined(LPC11U24FBD48_401)|| defined(LPC11U24FBD64_401) || defined(LPC11U24FHN33_401)
+#if defined(LPC11U24FET48_301) || defined(LPC11U24FHI33_301) || defined(LPC11U24FBD48_301) || defined(LPC11U24FBD48_401) || \
+    defined(LPC11U24FBD64_401) || defined(LPC11U24FHN33_401)
 #define LPC11U24
 #endif
 
@@ -60,8 +61,8 @@
 #define LPC_PARTNO_311
 #endif
 
-#if defined(LPC11U24FBD48_401) || defined(LPC11U24FBD64_401) || defined(LPC11U24FHN33_401) || defined(LPC11U35FBD48_401) || defined(LPC11U35FBD64_401) || defined(LPC11U35FHN33_401) || \
-    defined(LPC11U36) || defined(LPC11U37FBD48_401) || defined(LPC11U37FBD64_401)
+#if defined(LPC11U24FBD48_401) || defined(LPC11U24FBD64_401) || defined(LPC11U24FHN33_401) || defined(LPC11U35FBD48_401) || defined(LPC11U35FBD64_401) || \
+    defined(LPC11U35FHN33_401) || defined(LPC11U36) || defined(LPC11U37FBD48_401) || defined(LPC11U37FBD64_401)
 #define LPC_PARTNO_401
 #endif
 
@@ -181,16 +182,139 @@
 
 #if defined(LPC11Uxx)
 #define IRQ_VECTORS_COUNT             31
-#define GPIO_COUNT                    2
 #endif
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------- LPC18xx -----------------------------------------------------------------------------------------
+#if defined(LPC1810FET100) || defined(LPC1810FBD144)
+#define LPC1810
+#endif
+
+#if defined(LPC1812JBD144) || defined(LPC1812JET100)
+#define LPC1812
+#endif
+
+#if defined(LPC1813JBD144) || defined(LPC1813JET100)
+#define LPC1813
+#endif
+
+#if defined(LPC1815JBD144) || defined(LPC1815JET100)
+#define LPC1815
+#endif
+
+#if defined(LPC1817JBD144) || defined(LPC1817JET100)
+#define LPC1817
+#endif
+
+#if defined(LPC1820FET100) || defined(LPC1820FBD144)
+#define LPC1820
+#endif
+
+#if defined(LPC1822JBD144) || defined(LPC1822JET100)
+#define LPC1822
+#endif
+
+#if defined(LPC1823JBD144) || defined(LPC1823JET100)
+#define LPC1823
+#endif
+
+#if defined(LPC1825JBD144) || defined(LPC1825JET100)
+#define LPC1825
+#endif
+
+#if defined(LPC1827JBD144) || defined(LPC1827JET100)
+#define LPC1827
+#endif
+
+#if defined(LPC1830FET256) || defined(LPC1830FET180) || defined(LPC1830FET100) || defined(LPC1830FBD144)
+#define LPC1830
+#endif
+
+#if defined(LPC1833FET256) || defined(LPC1833JET256) || defined(LPC1833JBD144) || defined(LPC1833JET100)
+#define LPC1833
+#endif
+
+#if defined(LPC1837FET256) || defined(LPC1837JET256) || defined(LPC1837JBD144) || defined(LPC1837JET100)
+#define LPC1837
+#endif
+
+#if defined(LPC1850FET256) || defined(LPC1850FET180)
+#define LPC1850
+#endif
+
+#if defined(LPC1853FET256) || defined(LPC1853JET256) || defined(LPC1853JBD208)
+#define LPC1853
+#endif
+
+#if defined(LPC1857FET256) || defined(LPC1857JET256) || defined(LPC1857JBD208)
+#define LPC1857
+#endif
+
+#if defined(LPC1810) || defined(LPC1812) || defined(LPC1813) || defined(LPC1815) || defined(LPC1817) || defined(LPC1820) || defined(LPC1822) ||\
+    defined(LPC1823) || defined(LPC1825) || defined(LPC1827) || defined(LPC1830) || defined(LPC1833) || defined(LPC1837) || defined(LPC1850) ||\
+    defined(LPC1853) || defined(LPC1857)
+#define LPC18xx
+#define UARTS_COUNT                     4
+#define I2C_COUNT                       2
+#endif
+
+//SRAM0 only, SRAM1 and any external memory is not used in system configuration
+#if defined(LPC1812) || defined(LPC1813) || defined(LPC1815) || defined(LPC1817) || defined(LPC1822) || defined(LPC1823) || defined(LPC1825) ||\
+    defined(LPC1827) || defined(LPC1833) || defined(LPC1837) || defined(LPC1853) || defined(LPC1857)
+//32k
+#define SRAM_SIZE                       0x8000
+#endif
+
+#if defined(LPC1810)
+//64k
+#define SRAM_SIZE                       0x10000
+#endif
+
+#if defined(LPC1820) || defined(LPC1830) || defined(LPC1850)
+//96k
+#define SRAM_SIZE                       0x18000
+#endif
+
+//Bank A only, Bank B not used in system configuration
+#if defined(LPC1813) || defined(LPC1823) || defined(LPC1833) || defined(LPC1853)
+//256k
+#define FLASH_SIZE                      0x40000
+#endif
+
+#if defined(LPC1815) || defined(LPC1825)
+//384k
+#define FLASH_SIZE                      0x60000
+#endif
+
+#if defined(LPC1812) || defined(LPC1817) || defined(LPC1822) || defined(LPC1827) || defined(LPC1837) || defined(LPC1857)
+//512k
+#define FLASH_SIZE                      0x80000
+#endif
+
+#if defined(LPC1810) || defined(LPC1820) || defined(LPC1830) || defined(LPC1850)
+//compatibility mode
+#define FLASH_SIZE                      SRAM_SIZE
+#endif
+
+#if defined(LPC18xx)
+#define IRQ_VECTORS_COUNT               52
+#endif
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #if defined(LPC11Uxx)
-#define LPC
 #ifndef CORTEX_M0
 #define CORTEX_M0
 #endif //CORTEX_M0
 #endif //LPC11Uxx
+
+#if defined(LPC18xx)
+#ifndef CORTEX_M3
+#define CORTEX_M3
+#endif //CORTEX_M3
+#endif //LPC18xx
+
+#if defined(LPC11Uxx) || defined(LPC18xx)
+#define LPC
+#endif
 
 #if defined(CORTEX_M3) || defined(CORTEX_M0)
 #ifndef CORTEX_M
@@ -199,7 +323,7 @@
 #endif //defined(CORTEX_M3) || defined(CORTEX_M0)
 
 #if defined(LPC)
-
+//shadow area for 18xx
 #ifndef FLASH_BASE
 #define FLASH_BASE                0x00000000
 #endif //FLASH_BASE
@@ -222,6 +346,8 @@
 #else
 #include "LPC11Uxx.h"
 #endif //defined(LPC11U6x)
+#elif defined(LPC18xx)
+#include "LPC18xx.h"
 #endif //defined(LPC11Uxx)
 #endif //LPC
 
