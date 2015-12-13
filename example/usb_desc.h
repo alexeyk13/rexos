fe6d9b385ce55c6e51751a680bab25216720bcdc
Alexey Kramarenko <alexeyk13@yandex.ru>
2015-12-13 10:20:36 +0300

    IAD support

--- example/usb_desc.h
+++ example/usb_desc.h
@@ -8,17 +8,17 @@
 #define USB_DESC_H
 
 #include "../../rexos/userspace/usb.h"
-#include "../../rexos/userspace/cdc.h"
+#include "../../rexos/userspace/cdc_acm.h"
 
 #pragma pack(push, 1)
 
-const USB_DEVICE_DESCRIPTOR_TYPE __DEVICE_DESCRIPTOR = {
-    USB_DEVICE_DESCRIPTOR_SIZE,                                               /*bLength */
-    USB_DEVICE_DESCRIPTOR_INDEX,                                              /*bDescriptorType*/
-    0x0110,                                                                   /*bcdUSB */
-    0x02,                                                                     /*bDeviceClass*/
-    0x00,                                                                     /*bDeviceSubClass*/
-    0x00,                                                                     /*bDeviceProtocol*/
+const USB_DEVICE_DESCRIPTOR __DEVICE_DESCRIPTOR = {
+    sizeof(USB_DEVICE_DESCRIPTOR),                                            /*bLength */
+    USB_DEVICE_DESCRIPTOR_TYPE,                                               /*bDescriptorType*/
+    0x0200,                                                                   /*bcdUSB */
+    USB_MISCELLANEOUS_DEVICE_CLASS,                                           /*bDeviceClass*/
+    USB_MISCELLANEOUS_COMMON_SUBCLASS,                                        /*bDeviceSubClass*/
+    USB_IAD_PROTOCOL,                                                         /*bDeviceProtocol*/
     64,                                                                       /*bMaxPacketSize*/
     0x0525,                                                                   /*idVendor*/
     0xa4aa,                                                                   /*idProduct*/
@@ -30,23 +30,24 @@ const USB_DEVICE_DESCRIPTOR_TYPE __DEVICE_DESCRIPTOR = {
 };
 
 typedef struct {
-    USB_CONFIGURATION_DESCRIPTOR_TYPE configuration;
-    USB_INTERFACE_DESCRIPTOR_TYPE comm_interface;
+    USB_CONFIGURATION_DESCRIPTOR configuration;
+    USB_INTERFACE_ASSOCIATION_DESCRIPTOR comm_iad;
+    USB_INTERFACE_DESCRIPTOR comm_interface;
     CDC_HEADER_DESCRIPTOR_TYPE cdc_header;
     CDC_ACM_DESCRIPTOR_TYPE cdc_acm;
     CDC_UNION_DESCRIPTOR_TYPE cdc_union;
     uint8_t bSubordinateInterface0;
     CDC_CALL_MANAGEMENT_DESCRIPTOR_TYPE call_management;
-    USB_ENDPOINT_DESCRIPTOR_TYPE comm_endpoint;
-    USB_INTERFACE_DESCRIPTOR_TYPE data_interface;
-    USB_ENDPOINT_DESCRIPTOR_TYPE data_endpoints[2];
+    USB_ENDPOINT_DESCRIPTOR comm_endpoint;
+    USB_INTERFACE_DESCRIPTOR data_interface;
+    USB_ENDPOINT_DESCRIPTOR data_endpoints[2];
 } CONFIGURATION;
 
 const CONFIGURATION __CONFIGURATION_DESCRIPTOR = {
     //CONFIGURATION descriptor
     {
-        USB_CONFIGURATION_DESCRIPTOR_SIZE,                                  /*bLength*/
-        USB_CONFIGURATION_DESCRIPTOR_INDEX,                         		/*bDescriptorType*/
+        sizeof(USB_CONFIGURATION_DESCRIPTOR),                               /*bLength*/
+        USB_CONFIGURATION_DESCRIPTOR_TYPE,                                  /*bDescriptorType*/
         sizeof(CONFIGURATION),                                              /*wTotalLength*/
         2,                                                                  /*bNumInterfaces*/
         1,                                                                  /*bConfigurationValue*/
@@ -54,10 +55,21 @@ const CONFIGURATION __CONFIGURATION_DESCRIPTOR = {
         0x80,                                                               /*bmAttributes*/
         250                                                                 /*bMaxPower*/
     },
+    //COMM IAD
+    {
+        sizeof(USB_INTERFACE_ASSOCIATION_DESCRIPTOR),                       /*bLength*/
+        USB_INTERFACE_ASSOCIATION_DESCRIPTOR_TYPE,                          /*bDescriptorType*/
+        0,                                                                  /*bFirstInterface*/
+        2,                                                                  /*bInterfaceCount*/
+        CDC_COMM_INTERFACE_CLASS,                                           /*bFunctionClass*/
+        CDC_ACM,                                                            /*bFunctionSubClass*/
+        CDC_CP_V250,                                                        /*bFunctionProtocol*/
+        0                                                                   /*iFunction*/
+    },
     //comm INTERFACE descriptor
     {
-        USB_INTERFACE_DESCRIPTOR_SIZE,                                      /*bLength*/
-        USB_INTERFACE_DESCRIPTOR_INDEX,                                     /*bDescriptorType*/
+        sizeof(USB_INTERFACE_DESCRIPTOR),                                   /*bLength*/
+        USB_INTERFACE_DESCRIPTOR_TYPE,                                      /*bDescriptorType*/
         0,                                                                  /*bInterfaceNumber*/
         0,                                                                  /*bAlternateSetting*/
         1,                                                                  /*bNumEndpoints*/
@@ -98,17 +110,17 @@ const CONFIGURATION __CONFIGURATION_DESCRIPTOR = {
     },
     //comm endpoint descriptor
     {
-        USB_ENDPOINT_DESCRIPTOR_SIZE,                                       /*bLength*/
-        USB_ENDPOINT_DESCRIPTOR_INDEX,                                      /*bDescriptorType*/
+        sizeof(USB_ENDPOINT_DESCRIPTOR),                                    /*bLength*/
+        USB_ENDPOINT_DESCRIPTOR_TYPE,                                       /*bDescriptorType*/
         0x80 | 0x2,                                                         /*bEndpointAddress*/
         0x03,                                                               /*bmAttributes*/
-        16,                                                               	/*wMaxPacketSize*/
-        5                       											/*bInterval*/
+        16,                                                                 /*wMaxPacketSize*/
+        5                                                                   /*bInterval*/
     },
     //data INTERFACE descriptor
     {
-        USB_INTERFACE_DESCRIPTOR_SIZE,                                      /*bLength*/
-        USB_INTERFACE_DESCRIPTOR_INDEX,                                     /*bDescriptorType*/
+        sizeof(USB_INTERFACE_DESCRIPTOR),                                   /*bLength*/
+        USB_INTERFACE_DESCRIPTOR_TYPE,                                      /*bDescriptorType*/
         1,                                                                  /*bInterfaceNumber*/
         0,                                                                  /*bAlternateSetting*/
         2,                                                                  /*bNumEndpoints*/
@@ -120,33 +132,33 @@ const CONFIGURATION __CONFIGURATION_DESCRIPTOR = {
     //endpoints descriptors
     {
         {
-            USB_ENDPOINT_DESCRIPTOR_SIZE,                                       /*bLength*/
-            USB_ENDPOINT_DESCRIPTOR_INDEX,                                      /*bDescriptorType*/
-            0x80 | 0x1,                                                         /*bEndpointAddress*/
-            0x02,                                                               /*bmAttributes*/
-            64,                                                               	/*wMaxPacketSize*/
-            0                       											/*bInterval*/
+            sizeof(USB_ENDPOINT_DESCRIPTOR),                                /*bLength*/
+            USB_ENDPOINT_DESCRIPTOR_TYPE,                                   /*bDescriptorType*/
+            0x80 | 0x1,                                                     /*bEndpointAddress*/
+            0x02,                                                           /*bmAttributes*/
+            64,                                                             /*wMaxPacketSize*/
+            0                                                               /*bInterval*/
         },
         {
-            USB_ENDPOINT_DESCRIPTOR_SIZE,                                       /*bLength*/
-            USB_ENDPOINT_DESCRIPTOR_INDEX,                                      /*bDescriptorType*/
-            0x00 | 0x1,                                                         /*bEndpointAddress*/
-            0x02,                                                               /*bmAttributes*/
-            64,                                                                 /*wMaxPacketSize*/
-            0                                                                   /*bInterval*/
+            sizeof(USB_ENDPOINT_DESCRIPTOR),                                /*bLength*/
+            sizeof(USB_ENDPOINT_DESCRIPTOR),                                /*bDescriptorType*/
+            0x00 | 0x1,                                                     /*bEndpointAddress*/
+            0x02,                                                           /*bmAttributes*/
+            64,                                                             /*wMaxPacketSize*/
+            0                                                               /*bInterval*/
         }
     }
 };
 
 const char __STRING_WLANGS[] = {
-    1 * 2 + 2,                                                                      /*bLength*/
-    USB_STRING_DESCRIPTOR_INDEX,                                                /*bDescriptorType*/
-    0x09, 0x04                                                                  // 0x409 - English
+    1 * 2 + 2,                                                              /*bLength*/
+    USB_STRING_DESCRIPTOR_TYPE,                                             /*bDescriptorType*/
+    0x09, 0x04                                                              // 0x409 - English
 };
 
 const char __STRING_MANUFACTURER[] = {
-    6 * 2 + 2,                                                                      /*bLength*/
-    USB_STRING_DESCRIPTOR_INDEX,                                                /*bDescriptorType*/
+    6 * 2 + 2,                                                              /*bLength*/
+    USB_STRING_DESCRIPTOR_TYPE,                                             /*bDescriptorType*/
     'R', 0,
     'E', 0,
     'x', 0,
@@ -156,8 +168,8 @@ const char __STRING_MANUFACTURER[] = {
 };
 
 const char __STRING_PRODUCT[] = {
-    7 * 2 + 2,                                                                      /*bLength*/
-    USB_STRING_DESCRIPTOR_INDEX,                                                /*bDescriptorType*/
+    7 * 2 + 2,                                                              /*bLength*/
+    USB_STRING_DESCRIPTOR_TYPE,                                             /*bDescriptorType*/
     'U', 0,
     'S', 0,
     'B', 0,
@@ -168,8 +180,8 @@ const char __STRING_PRODUCT[] = {
 };
 
 const char __STRING_SERIAL[] = {
-    12 * 2 + 2,                                                                     /*bLength*/
-    USB_STRING_DESCRIPTOR_INDEX,                                                /*bDescriptorType*/
+    12 * 2 + 2,                                                             /*bLength*/
+    USB_STRING_DESCRIPTOR_TYPE,                                             /*bDescriptorType*/
     '0', 0,
     '1', 0,
     '2', 0,
@@ -185,8 +197,8 @@ const char __STRING_SERIAL[] = {
 };
 
 const char __STRING_DEFAULT[] = {
-    7 * 2 + 2,                                                                      /*bLength*/
-    USB_STRING_DESCRIPTOR_INDEX,                                                /*bDescriptorType*/
+    7 * 2 + 2,                                                              /*bLength*/
+    USB_STRING_DESCRIPTOR_TYPE,                                             /*bDescriptorType*/
     'D', 0,
     'e', 0,
     'f', 0,
