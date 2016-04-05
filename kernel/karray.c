30159fe84c6386576d237135cce9600c7f09b76e
Alexey Kramarenko <alexeyk13@yandex.ru>
2016-04-05 17:27:02 +0300

    karray, kso libraries

--- kernel/karray.c
+++ kernel/karray.c
@@ -6,49 +6,84 @@
 
 #include "karray.h"
 #include "kstdlib.h"
+#include "../lib/lib_lib.h"
 #include "../userspace/process.h"
 
 ARRAY* karray_create(ARRAY** ar, unsigned int data_size, unsigned int reserved)
 {
-    return ((const LIB_ARRAY*)__GLOBAL->lib[LIB_ID_ARRAY])->lib_array_create(ar, &__KSTD_MEM, data_size, reserved);
+    ARRAY* res;
+    LIB_ENTER
+    res = ((const LIB_ARRAY*)__GLOBAL->lib[LIB_ID_ARRAY])->lib_array_create(ar, &__KSTD_MEM, data_size, reserved);
+    LIB_EXIT
+    return res;
 }
 
 void karray_destroy(ARRAY** ar)
 {
+    LIB_ENTER
     ((const LIB_ARRAY*)__GLOBAL->lib[LIB_ID_ARRAY])->lib_array_destroy(ar, &__KSTD_MEM);
+    LIB_EXIT
 }
 
 void* karray_at(ARRAY* ar, unsigned int index)
 {
-    return ((const LIB_ARRAY*)__GLOBAL->lib[LIB_ID_ARRAY])->lib_array_at(ar, &__KSTD_MEM, index);
+    void* res;
+    LIB_ENTER
+    res = ((const LIB_ARRAY*)__GLOBAL->lib[LIB_ID_ARRAY])->lib_array_at(ar, &__KSTD_MEM, index);
+    LIB_EXIT
+    return res;
 }
 
 unsigned int karray_size(ARRAY* ar)
 {
-    return ((const LIB_ARRAY*)__GLOBAL->lib[LIB_ID_ARRAY])->lib_array_size(ar, &__KSTD_MEM);
+    unsigned int res;
+    LIB_ENTER
+    res = ((const LIB_ARRAY*)__GLOBAL->lib[LIB_ID_ARRAY])->lib_array_size(ar, &__KSTD_MEM);
+    LIB_EXIT
+    return res;
 }
 
 void* karray_append(ARRAY** ar)
 {
-    return ((const LIB_ARRAY*)__GLOBAL->lib[LIB_ID_ARRAY])->lib_array_append(ar, &__KSTD_MEM);
+    void* res;
+    LIB_ENTER
+    res = ((const LIB_ARRAY*)__GLOBAL->lib[LIB_ID_ARRAY])->lib_array_append(ar, &__KSTD_MEM);
+    LIB_EXIT
+    return res;
 }
 
 void* karray_insert(ARRAY** ar, unsigned int index)
 {
-    return ((const LIB_ARRAY*)__GLOBAL->lib[LIB_ID_ARRAY])->lib_array_insert(ar, &__KSTD_MEM, index);
+    void* res;
+    LIB_ENTER
+    res = ((const LIB_ARRAY*)__GLOBAL->lib[LIB_ID_ARRAY])->lib_array_insert(ar, &__KSTD_MEM, index);
+    LIB_EXIT
+    return res;
 }
 
 ARRAY* karray_clear(ARRAY** ar)
 {
-    return ((const LIB_ARRAY*)__GLOBAL->lib[LIB_ID_ARRAY])->lib_array_clear(ar, &__KSTD_MEM);
+    void* res;
+    LIB_ENTER
+    res = ((const LIB_ARRAY*)__GLOBAL->lib[LIB_ID_ARRAY])->lib_array_clear(ar, &__KSTD_MEM);
+    LIB_EXIT
+    return res;
 }
 
 ARRAY* karray_remove(ARRAY** ar, unsigned int index)
 {
-    return ((const LIB_ARRAY*)__GLOBAL->lib[LIB_ID_ARRAY])->lib_array_remove(ar, &__KSTD_MEM, index);
+    void* res;
+    LIB_ENTER
+    res = ((const LIB_ARRAY*)__GLOBAL->lib[LIB_ID_ARRAY])->lib_array_remove(ar, &__KSTD_MEM, index);
+    LIB_EXIT
+    return res;
 }
 
 ARRAY* karray_squeeze(ARRAY** ar)
 {
-    return ((const LIB_ARRAY*)__GLOBAL->lib[LIB_ID_ARRAY])->lib_array_squeeze(ar, &__KSTD_MEM);
+    void* res;
+    LIB_ENTER
+    res = ((const LIB_ARRAY*)__GLOBAL->lib[LIB_ID_ARRAY])->lib_array_squeeze(ar, &__KSTD_MEM);
+    LIB_EXIT
+    return res;
 }
---
+++ kernel/kso.h
@@ -0,0 +1,23 @@
+/*
+    RExOS - embedded RTOS
+    Copyright (c) 2011-2016, Alexey Kramarenko
+    All rights reserved.
+*/
+
+#ifndef KSO_H
+#define KSO_H
+
+#include "../userspace/so.h"
+
+SO* kso_create(SO* so, unsigned int data_size, unsigned int reserved);
+void kso_destroy(SO* so);
+//Remember: always re-fetch objects after kso_allocate
+HANDLE kso_allocate(SO* so);
+bool kso_check_handle(SO* so, HANDLE handle);
+void kso_free(SO* so, HANDLE handle);
+void* kso_get(SO* so, HANDLE handle);
+HANDLE kso_first(SO* so);
+HANDLE kso_next(SO* so, HANDLE prev);
+unsigned int kso_count(SO* so);
+
+#endif // KSO_H
