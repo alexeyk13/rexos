f6d82fbddbc64a403925b4f07def921ad98e0d08
Alexey Kramarenko <alexeyk13@yandex.ru>
2017-08-23 20:19:29 +0300

    LIS3DH userspace driver

--- userspace/drv/lis3dh.c
+++ userspace/drv/lis3dh.c
@@ -8,10 +8,10 @@
 #include "../io.h"
 #include "../stdlib.h"
 #include "../i2c.h"
-//remove me
-#include "../stdio.h"
+#include "../error.h"
+#include <string.h>
 
-#define LIS3DH_IO_SIZE                          8
+#define LIS_IO_SIZE                                         6
 
 typedef struct _LIS3DH {
     unsigned int i2c;
@@ -25,24 +25,104 @@ static void lis3dh_destroy(LIS3DH* lis3dh)
     free(lis3dh);
 }
 
-LIS3DH* lis3dh_open(unsigned int i2c, uint8_t sla)
+static void lis3dh_set_reg(LIS3DH* lis3dh, uint8_t reg, uint8_t value)
 {
+    *((char*)io_data(lis3dh->io)) = value;
+    lis3dh->io->data_size = 1;
+    i2c_write_addr(lis3dh->i2c, lis3dh->sla, reg, lis3dh->io);
+}
+
+static uint8_t lis3dh_get_reg(LIS3DH* lis3dh, uint8_t reg)
+{
+    if (i2c_read_addr(lis3dh->i2c, lis3dh->sla, reg, lis3dh->io, 1) < 1)
+        return 0xff;
+    return *((char*)io_data(lis3dh->io));
+}
+
+LIS3DH* lis3dh_open(unsigned int i2c, uint8_t sla, LIS_POWER_MODE power_mode, unsigned int rate_hz)
+{
+    uint8_t reg;
     LIS3DH* lis3dh = malloc(sizeof(LIS3DH));
-    int res;
     if (lis3dh == NULL)
         return NULL;
-    lis3dh->i2c = i2c;
-    lis3dh->sla = sla;
-    lis3dh->io = io_create(LIS3DH_IO_SIZE);
-    if (lis3dh->io == NULL)
-    {
-        lis3dh_destroy(lis3dh);
-        return NULL;
-    }
-    printf("tp0\n");
-    if (i2c_read_addr(lis3dh->i2c, lis3dh->sla, LIS3DH_REG_WHO_AM_I, lis3dh->io, 1) < 1)
-    {
-        printf("res: %d, data: %x\n", res, *((char*)io_data(lis3dh->io)));
-    }
-    return lis3dh;
+    do {
+        lis3dh->i2c = i2c;
+        lis3dh->sla = sla;
+        lis3dh->io = io_create(LIS_IO_SIZE + sizeof(I2C_STACK));
+        if (lis3dh->io == NULL)
+            break;
+
+        //check version and device present
+        if (lis3dh_get_reg(lis3dh, LIS_REG_WHO_AM_I) != LIS3DH_WHO_AM_I_VALUE)
+            break;
+
+        lis3dh_set_reg(lis3dh, LIS_REG_CTRL2, LIS_REG_CTRL3_FDS);
+
+        //exit from power down
+        reg = 0;
+        switch (rate_hz)
+        {
+        case 1:
+            reg = LIS_REG_CTRL1_ODR_1HZ;
+            break;
+        case 10:
+            reg = LIS_REG_CTRL1_ODR_10HZ;
+            break;
+        case 25:
+            reg = LIS_REG_CTRL1_ODR_25HZ;
+            break;
+        case 50:
+            reg = LIS_REG_CTRL1_ODR_50HZ;
+            break;
+        case 100:
+            reg = LIS_REG_CTRL1_ODR_100HZ;
+            break;
+        case 200:
+            reg = LIS_REG_CTRL1_ODR_200HZ;
+            break;
+        case 400:
+            reg = LIS_REG_CTRL1_ODR_400HZ;
+            break;
+        case 1600:
+            if (power_mode == LIS_LOW_POWER)
+                reg = LIS_REG_CTRL1_ODR_LP_1600HZ;
+            break;
+        case 1344:
+            if (power_mode != LIS_LOW_POWER)
+            reg = LIS_REG_CTRL1_ODR_1344HZ;
+            break;
+        case 5376:
+            if (power_mode == LIS_LOW_POWER)
+                reg = LIS_REG_CTRL1_ODR_LP_5376HZ;
+        default:
+            break;
+        }
+        if (reg == 0)
+        {
+            error(ERROR_INVALID_PARAMS);
+            break;
+        }
+        reg |= LIS_REG_CTRL1_ZEN | LIS_REG_CTRL1_YEN | LIS_REG_CTRL1_XEN;
+        if (power_mode == LIS_LOW_POWER)
+            reg |= LIS_REG_CTRL1_LPEN;
+        lis3dh_set_reg(lis3dh, LIS_REG_CTRL1, reg);
+
+        reg = 0;
+        if (power_mode == LIS_HIGH_RESOLUTION)
+            reg |= LIS_REG_CTRL4_HR;
+        lis3dh_set_reg(lis3dh, LIS_REG_CTRL4, reg);
+
+        return lis3dh;
+    } while (false);
+    lis3dh_destroy(lis3dh);
+    return NULL;
+}
+
+
+void lis3dh_get(LIS3DH* lis3dh, LIS_DATA* lis_data)
+{
+    //TODO: 1-time read after i2c fix
+    lis_data->x = ((int16_t)lis3dh_get_reg(lis3dh, LIS_REG_OUT_X_H) << 8) | lis3dh_get_reg(lis3dh, LIS_REG_OUT_X_L);
+    lis_data->y = ((int16_t)lis3dh_get_reg(lis3dh, LIS_REG_OUT_Y_H) << 8) | lis3dh_get_reg(lis3dh, LIS_REG_OUT_Y_L);
+    lis_data->z = ((int16_t)lis3dh_get_reg(lis3dh, LIS_REG_OUT_Z_H) << 8) | lis3dh_get_reg(lis3dh, LIS_REG_OUT_Z_L);
 }
--- userspace/drv/lis3dh.h
+++ userspace/drv/lis3dh.h
@@ -15,52 +15,104 @@
 #define LIS3DH_SLA_CS_HI                            0x19
 
 //registers list
-#define LIS3DH_REG_STATUS_AUX                       0x07
-#define LIS3DH_REG_ADC1_L                           0x08
-#define LIS3DH_REG_ADC1_H                           0x09
-#define LIS3DH_REG_ADC2_L                           0x0a
-#define LIS3DH_REG_ADC2_H                           0x0b
-#define LIS3DH_REG_ADC3_L                           0x0c
-#define LIS3DH_REG_ADC3_H                           0x0d
-#define LIS3DH_REG_WHO_AM_I                         0x0f
-#define LIS3DH_REG_CTRL0                            0x1e
-#define LIS3DH_REG_TEMP_CFG                         0x1f
-#define LIS3DH_REG_CTRL1                            0x20
-#define LIS3DH_REG_CTRL2                            0x21
-#define LIS3DH_REG_CTRL3                            0x22
-#define LIS3DH_REG_CTRL4                            0x23
-#define LIS3DH_REG_CTRL5                            0x24
-#define LIS3DH_REG_CTRL6                            0x25
-#define LIS3DH_REG_REFERENCE                        0x26
-#define LIS3DH_REG_STATUS                           0x27
-#define LIS3DH_REG_OUT_X_L                          0x28
-#define LIS3DH_REG_OUT_X_H                          0x29
-#define LIS3DH_REG_OUT_Y_L                          0x2a
-#define LIS3DH_REG_OUT_Y_H                          0x2b
-#define LIS3DH_REG_OUT_Z_L                          0x2c
-#define LIS3DH_REG_OUT_Z_H                          0x2d
-#define LIS3DH_REG_FIFO_CTRL                        0x2e
-#define LIS3DH_REG_FIFO_SRC                         0x2f
-#define LIS3DH_REG_INT1_CFG                         0x30
-#define LIS3DH_REG_INT1_SRC                         0x31
-#define LIS3DH_REG_INT1_THS                         0x32
-#define LIS3DH_REG_INT1_DURATION                    0x33
-#define LIS3DH_REG_INT2_CFG                         0x34
-#define LIS3DH_REG_INT2_SRC                         0x35
-#define LIS3DH_REG_INT2_THS                         0x36
-#define LIS3DH_REG_INT2_DURATION                    0x37
-#define LIS3DH_REG_CLICK_CFG                        0x38
-#define LIS3DH_REG_CLICK_SRC                        0x39
-#define LIS3DH_REG_CLICK_THS                        0x3a
-#define LIS3DH_REG_TIME_LIMIT                       0x3b
-#define LIS3DH_REG_TIME_LATENCY                     0x3c
-#define LIS3DH_REG_TIME_WINDOW                      0x3d
-#define LIS3DH_REG_ACT_THS                          0x3e
-#define LIS3DH_REG_ACT_DUR                          0x3f
+#define LIS_REG_STATUS_AUX                          0x07
+#define LIS_REG_ADC1_L                              0x08
+#define LIS_REG_ADC1_H                              0x09
+#define LIS_REG_ADC2_L                              0x0a
+#define LIS_REG_ADC2_H                              0x0b
+#define LIS_REG_ADC3_L                              0x0c
+#define LIS_REG_ADC3_H                              0x0d
+#define LIS_REG_WHO_AM_I                            0x0f
+#define LIS_REG_CTRL0                               0x1e
+#define LIS_REG_TEMP_CFG                            0x1f
+#define LIS_REG_CTRL1                               0x20
+#define LIS_REG_CTRL2                               0x21
+#define LIS_REG_CTRL3                               0x22
+#define LIS_REG_CTRL4                               0x23
+#define LIS_REG_CTRL5                               0x24
+#define LIS_REG_CTRL6                               0x25
+#define LIS_REG_REFERENCE                           0x26
+#define LIS_REG_STATUS                              0x27
+#define LIS_REG_OUT_X_L                             0x28
+#define LIS_REG_OUT_X_H                             0x29
+#define LIS_REG_OUT_Y_L                             0x2a
+#define LIS_REG_OUT_Y_H                             0x2b
+#define LIS_REG_OUT_Z_L                             0x2c
+#define LIS_REG_OUT_Z_H                             0x2d
+#define LIS_REG_FIFO_CTRL                           0x2e
+#define LIS_REG_FIFO_SRC                            0x2f
+#define LIS_REG_INT1_CFG                            0x30
+#define LIS_REG_INT1_SRC                            0x31
+#define LIS_REG_INT1_THS                            0x32
+#define LIS_REG_INT1_DURATION                       0x33
+#define LIS_REG_INT2_CFG                            0x34
+#define LIS_REG_INT2_SRC                            0x35
+#define LIS_REG_INT2_THS                            0x36
+#define LIS_REG_INT2_DURATION                       0x37
+#define LIS_REG_CLICK_CFG                           0x38
+#define LIS_REG_CLICK_SRC                           0x39
+#define LIS_REG_CLICK_THS                           0x3a
+#define LIS_REG_TIME_LIMIT                          0x3b
+#define LIS_REG_TIME_LATENCY                        0x3c
+#define LIS_REG_TIME_WINDOW                         0x3d
+#define LIS_REG_ACT_THS                             0x3e
+#define LIS_REG_ACT_DUR                             0x3f
+
+#define LIS_REG_CTRL1_XEN                           (1 << 0)
+#define LIS_REG_CTRL1_YEN                           (1 << 1)
+#define LIS_REG_CTRL1_ZEN                           (1 << 2)
+#define LIS_REG_CTRL1_LPEN                          (1 << 3)
+#define LIS_REG_CTRL1_ODR_POWER_DOWN                (0 << 4)
+#define LIS_REG_CTRL1_ODR_1HZ                       (1 << 4)
+#define LIS_REG_CTRL1_ODR_10HZ                      (2 << 4)
+#define LIS_REG_CTRL1_ODR_25HZ                      (3 << 4)
+#define LIS_REG_CTRL1_ODR_50HZ                      (4 << 4)
+#define LIS_REG_CTRL1_ODR_100HZ                     (5 << 4)
+#define LIS_REG_CTRL1_ODR_200HZ                     (6 << 4)
+#define LIS_REG_CTRL1_ODR_400HZ                     (7 << 4)
+#define LIS_REG_CTRL1_ODR_LP_1600HZ                 (8 << 4)
+#define LIS_REG_CTRL1_ODR_1344HZ                    (9 << 4)
+#define LIS_REG_CTRL1_ODR_LP_5376HZ                 (9 << 4)
+
+#define LIS_REG_CTRL3_HP_IA1                        (1 << 0)
+#define LIS_REG_CTRL3_HP_IA2                        (1 << 1)
+#define LIS_REG_CTRL3_HP_CLICK                      (1 << 2)
+#define LIS_REG_CTRL3_FDS                           (1 << 3)
+#define LIS_REG_CTRL3_HPCF                          (3 << 4)
+#define LIS_REG_CTRL3_HPM_NORMAL_RESET              (0 << 6)
+#define LIS_REG_CTRL3_HPM_REFERENCE                 (1 << 6)
+#define LIS_REG_CTRL3_HPM_NORMAL                    (2 << 6)
+#define LIS_REG_CTRL3_HPM_AUTORESET                 (3 << 6)
+
+
+#define LIS_REG_CTRL4_SIM                           (1 << 0)
+#define LIS_REG_CTRL4_ST_NORMAL                     (0 << 1)
+#define LIS_REG_CTRL4_ST_TEST1                      (1 << 1)
+#define LIS_REG_CTRL4_ST_TEST2                      (2 << 1)
+#define LIS_REG_CTRL4_HR                            (1 << 3)
+#define LIS_REG_CTRL4_FS_2G                         (0 << 4)
+#define LIS_REG_CTRL4_FS_4G                         (1 << 4)
+#define LIS_REG_CTRL4_FS_8G                         (2 << 4)
+#define LIS_REG_CTRL4_FS_16G                        (3 << 4)
+#define LIS_REG_CTRL4_BLE                           (1 << 6)
+#define LIS_REG_CTRL4_BDU                           (1 << 7)
+
+#define LIS3DH_WHO_AM_I_VALUE                       0x33
 
 typedef struct _LIS3DH LIS3DH;
 
-LIS3DH* lis3dh_open(unsigned int i2c, uint8_t sla);
+typedef enum {
+    LIS_LOW_POWER = 0,
+    LIS_NORMAL,
+    LIS_HIGH_RESOLUTION
+} LIS_POWER_MODE;
+
+typedef struct {
+    int16_t x, y, z;
+} LIS_DATA;
+
+LIS3DH* lis3dh_open(unsigned int i2c, uint8_t sla, LIS_POWER_MODE power_mode, unsigned int rate_hz);
+void lis3dh_get(LIS3DH* lis3dh, LIS_DATA* lis_data);
 //void lis3dh_close();
 
 
