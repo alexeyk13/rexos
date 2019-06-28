613309802a0795ca6d50b8d6a20bae1c58d6d2bd
Roma Jam <jam_roma@yahoo.com>
2019-06-28 18:12:48 +0300

    + Flash read/write function in driver have done. Not checked yet.

--- kernel/nrf/nrf_flash.c
+++ kernel/nrf/nrf_flash.c
@@ -19,7 +19,7 @@
 #include "../dbg.h"
 
 #define LOGICAL_SECTOR_SIZE             512
-#define FLASH_SECTOR_SIZE_WORDS         ((flash_page_size()) / sizeof(uint32_t))
+#define FLASH_PAGE_SIZE_WORDS         ((flash_page_size()) / sizeof(uint32_t))
 
 void nrf_flash_init(EXO* exo)
 {
@@ -44,7 +44,7 @@ unsigned int flash_total_size()
 static inline bool nrf_flash_is_sector_empty(unsigned int* addr)
 {
     unsigned int i;
-    for (i = 0; i < FLASH_SECTOR_SIZE_WORDS; ++i)
+    for (i = 0; i < FLASH_PAGE_SIZE_WORDS; ++i)
         if (addr[i] != 0xffffffff)
             return false;
     return true;
@@ -110,6 +110,80 @@ static inline void flash_write_word(uint32_t addr, uint32_t val)
     }
 }
 
+static inline void nrf_flash_page_read(EXO* exo, unsigned int offs, IO* io, unsigned int size)
+{
+    /* if storage not active */
+    if (!exo->flash.active)
+    {
+        kerror(ERROR_NOT_CONFIGURED);
+        return;
+    }
+
+    if ((offs + size) > flash_page_size())
+    {
+        kerror(ERROR_OUT_OF_RANGE);
+        return;
+    }
+
+    io_data_append(io, (void*)exo->flash.offset + offs, size);
+}
+
+static inline bool nrf_flash_page_erase(EXO* exo, unsigned int offs)
+{
+    /* if storage not active */
+    if (!exo->flash.active)
+    {
+        kerror(ERROR_NOT_CONFIGURED);
+        return false;
+    }
+
+    /* verify page aligment */
+    if((offs % flash_page_size()) != 0)
+    {
+        kerror(ERROR_INVALID_ALIGN);
+        return false;
+    }
+
+    /* erase page */
+    flash_erase_page(exo->flash.offset + offs);
+    return true;
+}
+
+static inline void nrf_flash_page_write(EXO* exo, unsigned int offs, uint8_t* buf, unsigned int size)
+{
+    unsigned int i = 0;
+    unsigned int page_size = flash_page_size();
+    /* if storage not active */
+    if (!exo->flash.active)
+    {
+        kerror(ERROR_NOT_CONFIGURED);
+        return;
+    }
+
+    if(size != page_size)
+    {
+        kerror(ERROR_INVALID_PARAMS);
+        return;
+    }
+
+    if(exo->flash.offset + offs > flash_total_size())
+    {
+        kerror(ERROR_OUT_OF_RANGE);
+        return;
+    }
+
+    /* dump page from flash in system buffer */
+    memcpy(exo->flash.page, buf, size);
+
+    /* erase page */
+    if(!nrf_flash_page_erase(exo, offs))
+        return;
+
+    /* write new data by words */
+    for(i = 0; i < page_size / sizeof(uint32_t); i++, offs += sizeof(uint32_t))
+        flash_write_word(exo->flash.offset + offs, *((uint32_t*)(exo->flash.page + (i * sizeof(uint32_t)))));
+}
+
 static inline void nrf_flash_read(EXO* exo,  HANDLE user, IO* io, unsigned int size)
 {
     /* TODO: temporary not supported */
@@ -154,55 +228,107 @@ static inline void nrf_flash_write(EXO* exo, HANDLE user, IO* io, unsigned int s
     kerror(ERROR_NOT_SUPPORTED);
     return;
     /* */
-}
 
-static inline void nrf_flash_sector_read(EXO* exo, unsigned int offs, IO* io, unsigned int size)
-{
-    if ((offs + size) > flash_page_size())
-    {
-        kerror(ERROR_OUT_OF_RANGE);
-        return;
-    }
-
-    io_data_append(io, (void*)exo->flash.offset + offs, size);
-}
-
-static inline void nrf_flash_sector_write(EXO* exo, unsigned int offs, IO* io, unsigned int size)
-{
-    unsigned int i = 0;
     unsigned int page_size = flash_page_size();
-    /* verify page aligment */
-    if((offs % page_size) != 0)
+    unsigned int base_addr, i, start_addr, sector_start_addr, data_size_cur;
+    void* buf;
+    STORAGE_STACK* stack = io_stack(io);
+    io_pop(io, sizeof(STORAGE_STACK));
+
+    /* if storage not active */
+    if (!exo->flash.active)
     {
-        kerror(ERROR_INVALID_ALIGN);
+        kerror(ERROR_NOT_CONFIGURED);
         return;
     }
-
-    if(size != page_size)
+    if ((stack->flags & STORAGE_MASK_MODE) == STORAGE_FLAG_ERASE_ONLY)
+        size *= LOGICAL_SECTOR_SIZE;
+    if ((user != exo->flash.user) || (size == 0))
+    {
+        kerror(ERROR_INVALID_PARAMS);
+        return;
+    }
+    if((stack->flags & STORAGE_OFFSET_INSTED_SECTOR) == 0)
+        stack->sector *= LOGICAL_SECTOR_SIZE;
+    base_addr = stack->sector + exo->flash.offset;
+    if (base_addr + size > FLASH_SIZE)
     {
         kerror(ERROR_INVALID_PARAMS);
         return;
     }
 
-    if(exo->flash.offset + offs > flash_total_size())
+    if (!(stack->flags & STORAGE_FLAG_IGNORE_ACTIVITY_ON_REQUEST))
+        nrf_flash_activity(exo);
+
+    if (stack->flags & (STORAGE_FLAG_ERASE_ONLY | STORAGE_OFFSET_INSTED_SECTOR))
     {
-        kerror(ERROR_OUT_OF_RANGE);
+        unsigned int flg = stack->flags;
+
+        stack->flags &= STORAGE_FLAG_ERASE_ONLY | STORAGE_OFFSET_INSTED_SECTOR;
+        size /= sizeof(uint32_t);
+
+        nrf_flash_page_write(exo, base_addr, io_data(io), size);
+
+        if(flg & STORAGE_FLAG_VERIFY)
+        {
+            if (memcmp(io_data(io), (void*)base_addr, size) != 0)
+                kerror(ERROR_CRC);
+        }
         return;
     }
 
-    /* dump page from flash in system buffer */
-    memcpy(exo->flash.page, io_data(io), size);
-    /* erase page */
-    flash_erase_page(exo->flash.offset + offs);
+    for (i = 0; i < size; i += data_size_cur)
+    {
+        start_addr = base_addr + i;
+        sector_start_addr = start_addr & ~(page_size - 1);
+        data_size_cur = page_size;
+        if (data_size_cur > size - i)
+            data_size_cur = size - i;
 
-    /* write new data by words */
-    for(i = 0; i < page_size / sizeof(uint32_t); i++, offs += sizeof(uint32_t))
-        flash_write_word(exo->flash.offset + offs, *((uint32_t*)(exo->flash.page + (i * sizeof(uint32_t)))));
-}
+        /* unaligned from start of flash page */
+        if (start_addr != sector_start_addr)
+        {
+            memcpy(exo->flash.page, (void*)sector_start_addr, start_addr - sector_start_addr);
+            if (data_size_cur + (start_addr - sector_start_addr) > page_size)
+                data_size_cur = page_size - (start_addr - sector_start_addr);
+        }
 
-static inline void nrf_flash_page_erase(EXO* exo, unsigned int offs)
-{
+        /* unaligned tail at sector end */
+        if (start_addr - sector_start_addr + data_size_cur < page_size)
+        {
+            memcpy(exo->flash.page + (start_addr - sector_start_addr) + data_size_cur,
+                   (void*)(start_addr + data_size_cur),
+                   page_size - (start_addr - sector_start_addr) - data_size_cur);
+        }
 
+        if (data_size_cur == page_size)
+        {
+            if ((stack->flags & STORAGE_MASK_MODE) == STORAGE_FLAG_ERASE_ONLY)
+            {
+                /* just erase page, that's all */
+                flash_erase_page(sector_start_addr);
+                /* go back */
+                return;
+            }
+            else
+                buf = (uint8_t*)io_data(io) + i;
+        }
+        else
+        {
+            /* copy data if data is not aligned in flash sector */
+            memcpy(exo->flash.page + (start_addr - sector_start_addr), (uint8_t*)io_data(io) + i, data_size_cur);
+            buf = exo->flash.page;
+        }
+
+        nrf_flash_page_write(exo, sector_start_addr, buf, page_size);
+
+        /* verify data in flash */
+        if (memcmp(buf, (void*)sector_start_addr, page_size) != 0)
+        {
+            kerror(ERROR_CRC);
+            return;
+        }
+    }
 }
 
 static inline void nrf_flash_open(EXO* exo, HANDLE user, unsigned int offs)
@@ -311,10 +437,10 @@ void nrf_flash_request(EXO* exo, IPC* ipc)
             nrf_flash_request_notify_activity(exo, ipc->process);
             break;
         case FLASH_PAGE_READ:
-            nrf_flash_sector_read(exo, ipc->param1, (IO*)ipc->param2, ipc->param3);
+            nrf_flash_page_read(exo, ipc->param1, (IO*)ipc->param2, ipc->param3);
             break;
         case FLASH_PAGE_WRITE:
-            nrf_flash_sector_write(exo, ipc->param1, (IO*)ipc->param2, ipc->param3);
+            nrf_flash_page_write(exo, ipc->param1, io_data((IO*)ipc->param2), ipc->param3);
             break;
         case FLASH_PAGE_ERASE:
             nrf_flash_page_erase(exo, ipc->param1);
